9de501463b692ecf1630b1a5568354e28a7a4977
SasaB86 <sasabocin86@gmail.com>
2020-06-17 16:03:15 +0200

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "sasa-notes-app-bucket"
+    },
+    apiGateway: {
+      REGION: "eu-central-1",
+      URL: "https://gd1mlvcz33.execute-api.eu-central-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: " eu-central-1",
+      USER_POOL_ID: "eu-central-1_BHIaHtQ9f",
+      APP_CLIENT_ID: "6p2e112j0vb5mihjaeaevk6b22",
+      IDENTITY_POOL_ID: "eu-central-1:694aa9db-23c2-4b7d-83c5-7755eae44ae8"
+    }
+  };
