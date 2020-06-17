a8393fd3122fbe7868b1687946c6c1d57f44f6a8
SasaB86 <sasabocin86@gmail.com>
2020-06-17 19:43:43 +0200

    added funcionality to app

--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
-      REGION: "us-east-1",
-      BUCKET: "sasa-notes-app-bucket"
+      REGION: "eu-central-1",
+      BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-1fh4peetmc1my"
     },
     apiGateway: {
       REGION: "eu-central-1",
       URL: "https://gd1mlvcz33.execute-api.eu-central-1.amazonaws.com/prod"
     },
     cognito: {
-      REGION: " eu-central-1",
+      REGION: "eu-central-1",
       USER_POOL_ID: "eu-central-1_BHIaHtQ9f",
       APP_CLIENT_ID: "6p2e112j0vb5mihjaeaevk6b22",
-      IDENTITY_POOL_ID: "eu-central-1:694aa9db-23c2-4b7d-83c5-7755eae44ae8"
+      IDENTITY_POOL_ID: "eu-central-1:775d0137-deeb-4583-8a3b-1b7c38e16e65"
     }
   };
